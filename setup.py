e193521b88465f3703380a1d873b78938d03cd22
nishshanka20 <92959720+nishshanka20@users.noreply.github.com>
2024-07-22 16:54:16 +0530

    data ingestion notebook added

--- setup.py
+++ setup.py
@@ -8,18 +8,19 @@ __version__ ="0.0.0"
 REPO_NAME="text_summerizer_project"
 AUTHER_USER_NAME="nishshanka20"
 SRC_REPO="textSummerizer"
-AUTHER_EMAIL_URL="nishshankawla@gmail.com" 
+AUTHER_EMAIL ="nishshankawla@gmail.com" 
 
 setuptools.setup(
-    name=REPO_NAME,
+    name=SRC_REPO,
     version=__version__,
     author=AUTHER_USER_NAME,
-    author_email=AUTHER_EMAIL_URL,
+    author_email=AUTHER_EMAIL,
     description="A small package to summerize text",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/" + AUTHER_USER_NAME + "/" + REPO_NAME,
     project_urls={"Bug Tracker":f"https://github.com/{AUTHER_USER_NAME}/{REPO_NAME}/issues",
 },
-    packages=setuptools.find_packages(),
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
 )
